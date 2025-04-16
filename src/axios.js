19be8d81854755f17964bf6cee1ae8d86da344b2
Test-codes-03 <ksudarshanksudarshan@gmail.com>
2025-04-16 20:17:32 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,6 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL:'https://jwlbackend-praveen.onrender.com',
+  baseURL:'https://gold-backend-gmk0.onrender.com',
   withCredentials: true,
 });
